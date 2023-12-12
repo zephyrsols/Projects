9e69a6d170cc6ca8b4da54d4b07a92d163b4ecc4
Ali Hamza <alihamza00053@gmail.com>
2023-12-12 15:54:12 +0500

    new

---
+++ Screen Mirroring/app/src/main/java/com/example/screenmirroring/ImageAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.screenmirroring
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageAdapter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_adapter)
+    }
+}
---
+++ Screen Mirroring/app/src/main/java/com/example/screenmirroring/Images.kt
@@ -0,0 +1,11 @@
+package com.example.screenmirroring
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Images : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_images)
+    }
+}
